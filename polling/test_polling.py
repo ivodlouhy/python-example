6905cf8b86ff7bcde0ad51c2e7387fd5b5ffec9b
Ivo Dlouhy <ivo.dlouhy@vertice.one>
2024-08-27 15:07:42 +0200

    Format

--- polling/test_polling.py
+++ polling/test_polling.py
@@ -9,44 +9,46 @@ from playwright.sync_api import Page, expect
 log = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG)
 
-def test_assert_is_visible_no_wait(page: Page):
 
+def test_assert_is_visible_no_wait(page: Page):
     # Fails → Locator.is_visible() doesn't wait
     # See: https://playwright.dev/python/docs/api/class-locator#locator-is-visible
 
     page.goto("http://localhost:8000/timeout-3s.html")
     button_locator = page.locator('button').filter(has_text='Button')
+
     assert button_locator.is_visible() == True, "Expected button to be visible"
 
 
 def test_assert_is_visible_wait_sleep(page: Page):
-
     # Passes → Wait using time.sleep is not a good practice
 
     page.goto("http://localhost:8000/timeout-3s.html")
-    time.sleep(4)
     button_locator = page.locator('button').filter(has_text='Button')
+
+    time.sleep(4)
+
     assert button_locator.is_visible() == True, "Expected button to be visible"
+
 
 @pytest.mark.parametrize('execution_number', range(7))
 def test_assert_is_visible_wait_sleep_random(page: Page, execution_number):
-
     # Intermittently fails → Wait using time.sleep is not a good practice as delays may vary
 
     page.goto("http://localhost:8000/timeout-random.html")
-    time.sleep(4)
     button_locator = page.locator('button').filter(has_text='Button')
+
+    time.sleep(4)
+
     button_is_visible = button_locator.is_visible()
     assert button_is_visible == True, "Expected button to be visible"
 
 
 def test_assert_is_visible_poling(page: Page):
-
     # Passes → is_visible with polling will wait
 
     page.goto("http://localhost:8000/timeout-random.html")
     button_locator = page.locator('button').filter(has_text='Button')
-
 
     polling.poll(
         lambda: button_locator.is_visible(),
@@ -58,35 +60,35 @@ def test_assert_is_visible_poling(page: Page):
     button_locator.click()
 
 
-    # button_is_visible = button_locator.is_visible()
-    # assert button_is_visible == True, "Expected button to be visible"
-
-
 def test_expect_to_be_visible(page: Page):
-
     # Passes → expect.to_be_visible() will wait
     # See: https://playwright.dev/python/docs/api/class-locatorassertions#locator-assertions-to-be-visible
 
     page.goto("http://localhost:8000/timeout-3s.html")
     button_locator = page.locator('button').filter(has_text='Button')
+
     expect(button_locator).to_be_visible()
+
+    button_locator.click()
 
 
 def test_expect_to_be_visible_timeout(page: Page):
-
     # Passes -> larger timeout for to_be_visible()
 
     page.goto("http://localhost:8000/timeout-7s.html")
     button_locator = page.locator('button').filter(has_text='Button')
+
     expect(button_locator).to_be_visible(timeout=15000)
+
+    button_locator.click()
 
 
 def test_expect_to_be_visible_timeout_polling(page: Page):
-
     # Passes → Expect to be visible with polling
 
     page.goto("http://localhost:8000/timeout-7s.html")
     button_locator = page.locator('button').filter(has_text='Button')
+
     polling.poll(
         lambda: expect(button_locator).to_be_visible(),
         step=1,
@@ -94,22 +96,24 @@ def test_expect_to_be_visible_timeout_polling(page: Page):
         check_success=lambda _: button_locator.is_visible(),
         ignore_exceptions=(AssertionError,)
     )
+
     button_locator.click()
 
 
 @pytest.mark.parametrize('execution_number', range(7))
 def test_expect_to_be_visible_intermittent(page: Page, execution_number):
-
     # Intermittently fails → hidden logic or intermittent behaviour
 
     page.goto("http://localhost:8000/timeout-3s-intermittent.html")
     button_locator = page.locator('button').filter(has_text='Button')
+
     expect(button_locator).to_be_visible(timeout=5000)
+
+    button_locator.click()
 
 
 def test_expect_to_be_visible_timeout_reload(page: Page):
-
-    # Passes → Handles intermittent behaviour with reloads
+    # Passes → Handles intermittent behaviour with reloads every time
 
     page.goto("http://localhost:8000/timeout-3s-intermittent.html")
     button_locator = page.locator('button').filter(has_text='Button')
@@ -118,7 +122,7 @@ def test_expect_to_be_visible_timeout_reload(page: Page):
         log.debug("Reloading page")
         page.reload()
         expect(button_locator).to_be_visible()
-        return True # or use check_success
+        return True  # or use check_success
 
     polling.poll(
         lambda: check(),
@@ -126,22 +130,29 @@ def test_expect_to_be_visible_timeout_reload(page: Page):
         timeout=30,
         ignore_exceptions=(AssertionError,)
     )
+
     button_locator.click()
 
-def test_assert_is_visible_timeout_reload(page: Page):
 
-    # Passes → Handles intermittent behaviour with reloads
+def test_expect_to_be_visible_timeout_reload_optimal(page: Page):
+    # Passes → Handles intermittent behaviour with reloads after first try
 
-    page.goto("http://localhost:8000/intermittent.html")
+    page.goto("http://localhost:8000/timeout-3s-intermittent.html")
     button_locator = page.locator('button').filter(has_text='Button')
 
     def check():
-        page.reload()
-        return button_locator.is_visible()
+        try:
+            expect(button_locator).to_be_visible()
+            return True  # or use a custom check_success
+        except AssertionError:
+            page.reload()
+            return False  # or use a custom check_success
 
     polling.poll(
         lambda: check(),
         step=1,
         timeout=30,
+        ignore_exceptions=(AssertionError,)
     )
+
     button_locator.click()
