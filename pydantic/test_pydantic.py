a832523b6d3a7346ae77e0f973f088f9c510d2cf
Ivo Dlouhy <ivo.dlouhy@vertice.one>
2024-08-27 15:19:51 +0200

    Add Pydantic

--- pydantic/test_pydantic.py
+++ pydantic/test_pydantic.py
@@ -1,2 +1,20 @@
+from pydantic import BaseModel
+
 def test_assert_is_visible_no_wait():
-    pass
+
+    class Dog(BaseModel):
+        name: str
+        age: int
+        breed: str
+
+    dog = Dog(name='Pluto', age=5, breed='Retriever')
+    assert dog.name == 'Pluto'
+
+
+def test_assert_is_visible_wait_sleep():
+    dog = {
+        'name': 'Pluto',
+        'age': 5,
+        'breed': 'Retriever'
+    }
+    assert dog['name'] == 'Pluto'
